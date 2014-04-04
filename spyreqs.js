c3da4ceb2202456c46a3ead0e95e9c7f6dede8b0
kostas savvopoulos <ksavvopoulos@gmail.com>
2014-04-04 16:15:04 +0300

    get Node

--- spyreqs.js
+++ spyreqs.js
@@ -806,7 +806,7 @@
                 return getAsync(url);
             },
             getHostNode:function(){
-                var url = baseUrl + "web/navigation/GetNodeById(2215)/" + targetStr;
+                var url = baseUrl + "web/navigation/GetNodeById(2215)?" + targetStr;
                 return getAsync(url);
             }
         },
